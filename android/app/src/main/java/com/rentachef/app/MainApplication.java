7fca47c9692a2a6979f85064c8b38e2d97c39c11
aruntummala <fivearun@gmail.com>
2021-07-07 20:04:38 -0500

    build: renamed app

--- android/app/src/main/java/com/rentachef/app/MainApplication.java
+++ android/app/src/main/java/com/rentachef/app/MainApplication.java
@@ -1,4 +1,4 @@
-package com.fooddelivery_pro;
+package com.rentachef.app;
 
 import android.app.Application;
 import android.content.Context;
@@ -63,7 +63,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.fooddelivery_pro.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.rentachef.app.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
--- ios/rentachefTests/rentachefTests.m
+++ ios/rentachefTests/rentachefTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface FoodDelivery_ProTests : XCTestCase
+@interface rentachefTests : XCTestCase
 
 @end
 
-@implementation FoodDelivery_ProTests
+@implementation rentachefTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
