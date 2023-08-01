ae372826989391c8e4f995668cdf2d3291a7c866
nicosoutn2014 <nicolasmp920@gmail.com>
2023-08-01 15:22:17 -0300

    removed npm

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -9,13 +9,9 @@ set -e
 set -x
 
 # Install dependencies using Homebrew. This is MUST! Do not delete.
-brew install node@16 cocoapods fastlane vips pipenv
-
-
-echo 'export PATH="/usr/local/opt/node@16/bin:$PATH"' >> ~/.zshrc
+brew install yarn cocoapods fastlane
 
 # Install yarn and pods dependencies.
-ls && cd .. && cd .. && npm install && npm run ios-device
 # If you're using Flutter or Swift 
 # just install pods by "pod install" command 
 ls && cd ios && yarn && pod install
