7a1fcf493b01bfd270977973b3de496820432a55
nicosoutn2014 <nicolasmp920@gmail.com>
2023-08-01 11:47:06 -0300

    install dependencies and move assets to ios folder

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -9,10 +9,10 @@
  set -x
 
  # Install dependencies using Homebrew. This is MUST! Do not delete.
- brew install node yarn cocoapods fastlane
+ brew install node cocoapods fastlane
 
  # Install yarn and pods dependencies.
- ls && cd .. && npm run ios-device
+ ls && cd .. && npm install && npm run ios-device
  # If you're using Flutter or Swift 
  # just install pods by "pod install" command 
  ls && cd ios && yarn && pod install
