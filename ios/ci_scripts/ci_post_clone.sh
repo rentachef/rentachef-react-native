f362a6c07df9bb8788d071a3bf110e602f3acef1
nicosoutn2014 <nicolasmp920@gmail.com>
2023-08-02 09:33:11 -0300

    main.jsbundle fix attempt

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -9,7 +9,9 @@ set -e
 set -x
 
 # Install dependencies using Homebrew. This is MUST! Do not delete.
-brew install node yarn cocoapods fastlane
+brew install node yarn cocoapods fastlane react-native-cli
+brew link node
+brew link react-native-cli
 
 # Install yarn and pods dependencies.
 # If you're using Flutter or Swift 
--- ios/ci_scripts/ci_pre_xcodebuild.sh
+++ ios/ci_scripts/ci_pre_xcodebuild.sh
@@ -2,7 +2,7 @@
 
 echo "🧩 Stage: PRE-Xcode Build is activated .... "
 
-cd .. && yarn add -D react-native-cli && react-native bundle --entry-file index.js --platform ios --dev false --bundle-output ios/main.jsbundle
+cd .. && react-native bundle --entry-file index.js --platform ios --dev false --bundle-output ios/main.jsbundle
 
 echo "🎯 Stage: PRE-Xcode Build is DONE .... "
 
