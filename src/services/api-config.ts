1540c8a5e2daee87aa4e41cb6585314521f723ee
nicosoutn2014 <nicolasmp920@gmail.com>
2024-09-30 09:58:40 -0300

    api production url

--- src/services/api-config.ts
+++ src/services/api-config.ts
@@ -19,8 +19,8 @@ const DEFAULT_TIMEOUT = 30000
  * The default configuration for the app.
  */
 export const DEFAULT_API_CONFIG: ApiConfig = {
-  url: 'http://rentachefdevelop-env.eba-rzrqkwm8.us-east-1.elasticbeanstalk.com/api/v1/', //develop
-  //url: 'http://rentachef-production-env.eba-rzrqkwm8.us-east-1.elasticbeanstalk.com/api/v1/', //production
+  //url: 'http://rentachefdevelop-env.eba-rzrqkwm8.us-east-1.elasticbeanstalk.com/api/v1/', //develop
+  url: 'http://rentachef-production-env.eba-rzrqkwm8.us-east-1.elasticbeanstalk.com/api/v1/', //production
   //url: 'http://localhost:3000/api/v1/', //local
   timeout: DEFAULT_TIMEOUT,
 }
